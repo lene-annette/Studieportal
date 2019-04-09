1d96e81404120b69cfa6a307a509a62d25bdaa87
lene-annette <lene.a.skov@gmail.com>
2019-04-09 15:31:02 +0200

    Tables and helper for SQLite database added

---
+++ app/src/main/java/dk/nytmodultest/studieportal/data/db/StudentDbHelper.kt
@@ -0,0 +1,33 @@
+package dk.nytmodultest.studieportal.data.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import dk.nytmodultest.studieportal.ui.App
+import org.jetbrains.anko.db.*
+
+class StudentDbHelper(ctx: Context = App.instance): ManagedSQLiteOpenHelper(ctx,StudentDbHelper.DB_NAME,null,StudentDbHelper.DB_VERSION) {
+    companion object {
+        const val DB_NAME = "student.db"
+        const val DB_VERSION = 1
+        val instance by lazy {StudentDbHelper()}
+    }
+
+    override fun onCreate(db: SQLiteDatabase){
+        db.createTable(StudentTable.NAME, true,
+            StudentTable.ID to INTEGER + PRIMARY_KEY,
+            StudentTable.FIRSTNAME to TEXT,
+            StudentTable.LASTNAME to TEXT,
+            StudentTable.USERNAME to TEXT,
+            StudentTable.EMAIL to TEXT,
+            StudentTable.PHONENR to TEXT,
+            StudentTable.CITIZENSHIP to TEXT,
+            StudentTable.LAST5YCOUNTRY to TEXT,
+            StudentTable.BIRTHCOUNTRY to TEXT,
+            StudentTable.MOTHERTONGUE to TEXT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(StudentTable.NAME,true)
+        onCreate(db)
+    }
+}
