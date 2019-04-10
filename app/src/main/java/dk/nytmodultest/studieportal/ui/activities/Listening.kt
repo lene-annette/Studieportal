80fdfcc18667e532dc8a6228be31ca25b5ed34e4
Weiqifan1 <cmlykke@hotmail.com>
2019-04-10 12:08:46 +0200

    the listening button on Profile.kt can now go to a listening page (the page only displays a textview)

---
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
@@ -0,0 +1,14 @@
+package dk.nytmodultest.studieportal.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import dk.nytmodultest.studieportal.R
+
+class Listening : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listening)
+    }
+
+}
--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Profile.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Profile.kt
@@ -1,7 +1,9 @@
 package dk.nytmodultest.studieportal.ui.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import dk.nytmodultest.studieportal.R
 import dk.nytmodultest.studieportal.data.Request
 import kotlinx.android.synthetic.main.activity_profile.*
@@ -16,12 +18,26 @@ class Profile : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         val url = "https://nytmodultest.dk/modultest-api/api/students/1"
+
         doAsync(){
             val result = Request(url).run()
             uiThread{
                 longToast("Request performed")
                 profileInfo.text = "Hello ${result.firstName}!"
+                //globalResult = result
             }
         }
+
+
+        //listeningBtn.setOnClickListener {
+        //    startActivity(Intent(this, Listening::class.java))
+        //}
+
+        // find the botton to the listening page
+        val listeningBtn = findViewById(R.id.listeningBtn)as Button
+        listeningBtn.setOnClickListener{
+            startActivity(Intent(this, Listening::class.java))
+        }
+
     }
 }
