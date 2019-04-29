cc5d9636d0ac4e3ebcbdd6f3d5b9a88d6f8099fe
Weiqifan1 <cmlykke@hotmail.com>
2019-04-29 13:01:59 +0200

    added a listening activity and an xml

---
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
@@ -0,0 +1,11 @@
+package dk.nytmodultest.studieportal.ui.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class Listening : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
