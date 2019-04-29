394bdba737bfc9a8d4dc81c7a9062db9d9554439
lene-annette <lene.a.skov@gmail.com>
2019-04-29 15:21:34 +0200

    SharedPreferences is, sort of, implemented, and works

--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/MainActivity.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/MainActivity.kt
@@ -4,15 +4,31 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import dk.nytmodultest.studieportal.R
+import dk.nytmodultest.studieportal.extensions.DelegatesExt
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val ID_TOKEN = "idToken"
+        const val DEFAULT_TOKEN = "none"
+    }
+
+    private var token: String
+        by DelegatesExt.preference(this,ID_TOKEN,DEFAULT_TOKEN)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if(token.equals(DEFAULT_TOKEN)){
+            setContentView(R.layout.activity_main)
+        }else{
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+
 
         loginBtn.setOnClickListener {
+            token = "I'm a token!"
             startActivity(Intent(this, ProfileActivity::class.java))
         }
     }
