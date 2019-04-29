6e0dc88414e150feae6f3492e936e0bb734affd8
Weiqifan1 <cmlykke@hotmail.com>
2019-04-29 13:47:10 +0200

    async virker! (med hardcoded string) nu skal jeg bruge RequestStudentCommand

---
+++ app/src/main/java/dk/nytmodultest/studieportal/domain/commands/RequestMultiChoiceCommand.kt
@@ -0,0 +1,10 @@
+package dk.nytmodultest.studieportal.domain.commands
+
+
+class RequestMultiChoiceCommand() {
+
+    fun execute(): String{
+        return "Hello from RequestMultiChoiceCommand"
+    }
+
+}
--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
@@ -1,7 +1,11 @@
 package dk.nytmodultest.studieportal.ui.activities
 
+//import dk.nytmodultest.studieportal.domain.commands.RequestStudentCommand
+import dk.nytmodultest.studieportal.domain.commands.RequestMultiChoiceCommand
 import kotlinx.android.synthetic.main.activity_listening.*
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import dk.nytmodultest.studieportal.R
@@ -10,7 +14,16 @@ class Listening : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listening)
+
+        doAsync(){
+            val result = "hello world - studieprotal"//RequestMultiChoiceCommand.execute()
+            uiThread{
+                act_textcontent.text = "${result}"
+            }
+        }
     }
+
+
 
     /*
     doAsync(){
