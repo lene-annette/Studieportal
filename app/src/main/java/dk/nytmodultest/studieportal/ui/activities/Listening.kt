13fea8ec83e63bac9f938ff54ade08ae8cf69179
Weiqifan1 <cmlykke@hotmail.com>
2019-05-01 11:12:09 +0200

    recyclerview layout looks ok. kode works. I am in the process of making the recycler view handler

--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
@@ -5,16 +5,10 @@ import dk.nytmodultest.studieportal.domain.model.*
 import android.os.AsyncTask
 import kotlinx.android.synthetic.main.activity_listening.*
 import android.os.Bundle
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
-import android.text.method.ScrollingMovementMethod
 import com.google.gson.Gson
 import dk.nytmodultest.studieportal.R
-import dk.nytmodultest.studieportal.domain.commands.RequestMultiChoiceCommand
 import java.net.URL
-import kotlin.Exception
 
 class Listening : AppCompatActivity(){
 
@@ -25,16 +19,18 @@ class Listening : AppCompatActivity(){
 
         override fun doInBackground(vararg params: String?): String {
             val databaseGet: String
-            try{
+            databaseGet =  URL("http://10.0.2.2:8000/api/findQuestionsByExerciseId/1").readText()
+            //try{
                 //use "10.0.2.2" instead of localhost (ottherwise you get an exception)
-                databaseGet =  URL("http://10.0.2.2:8000/api/findQuestionsByExerciseId/1").readText()
-            } catch (e: Exception) {
+                //databaseGet =  URL("http://10.0.2.2:8000/api/findQuestionsByExerciseId/1").readText()
+            //} catch (e: Exception) {
                 //return e.toString()
-                return "Database error"
-            }
+                //return "Database error"
+            //}
+            return databaseGet
 
-            val article = Gson().fromJson(databaseGet, Exercise::class.java)
-            return article.toString()
+            //val article = Gson().fromJson(databaseGet, Exercise::class.java)
+            //return article
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
@@ -43,8 +39,13 @@ class Listening : AppCompatActivity(){
 
         override fun onPostExecute(result: String?) {
             //super.onPostExecute(result)
-            val myresult = result
-            act_textcontent.text = "${myresult}"
+            val myresult: Exercise = Gson().fromJson(result, Exercise::class.java)
+            val media: String = myresult.media
+            val studentInstructions: String = myresult.studentInstructions
+
+            XMLmedia.text = media
+            XMLstudentIns.text = studentInstructions
+            //act_textcontent.text = "${myresult}"
         }
 
     }
@@ -55,7 +56,7 @@ class Listening : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listening)
-        act_textcontent.setMovementMethod(ScrollingMovementMethod());
+        //act_textcontent.setMovementMethod(ScrollingMovementMethod());
         GetListeningExercise().execute()
 
     }
