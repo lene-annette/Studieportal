195aa66dcd9a537a65a4451eb9dbc60cd0e5b0c9
Weiqifan1 <cmlykke@hotmail.com>
2019-05-02 09:57:53 +0200

    ready to try out recyclerView

--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/ChrBranch.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/ChrBranch.kt
@@ -8,5 +8,8 @@ class ChrBranch : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chrbranch)
+
+        
+
     }
 }
