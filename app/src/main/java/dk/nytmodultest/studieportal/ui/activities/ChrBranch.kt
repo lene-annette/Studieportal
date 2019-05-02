45aa4c74d9313ffb6490e7b13e7380788605806f
Weiqifan1 <cmlykke@hotmail.com>
2019-05-02 10:58:32 +0200

    recycleView virker i Studioportal projektet (men i forkert klasse) Jeg skal nu få async koden ind i projektet igen

--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/ChrBranch.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/ChrBranch.kt
@@ -2,6 +2,9 @@ package dk.nytmodultest.studieportal.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
 import dk.nytmodultest.studieportal.R
 
 class ChrBranch : AppCompatActivity(){
@@ -9,7 +12,17 @@ class ChrBranch : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chrbranch)
 
-        
+        val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
+        val users = ArrayList<User>()
+        users.add(User("Belal Khan", "Ranchi, India"))
+        users.add(User("Belal Khan", "Ranchi, India"))
+        users.add(User("Belal Khan", "Ranchi, India"))
+        users.add(User("Belal Khan", "Ranchi, India"))
+        users.add(User("Belal Khan", "Ranchi, India"))
+
+        val adapter = CustomAdapter(users)
+        recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/User.kt
@@ -0,0 +1,6 @@
+package dk.nytmodultest.studieportal.ui.activities
+
+data class User (
+    val name: String,
+    val address: String
+)
