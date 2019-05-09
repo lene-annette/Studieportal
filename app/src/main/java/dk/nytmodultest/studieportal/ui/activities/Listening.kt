1fe2af6aab651f6e03ce10d0936f5dbc4bd0ec56
Weiqifan1 <cmlykke@hotmail.com>
2019-05-09 19:15:35 +0200

    Audioplayback works, but only with testing url and it start slowly

--- app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/ui/activities/Listening.kt
@@ -10,8 +10,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import com.google.gson.Gson
+import dk.nytmodultest.studieportal.R
+import dk.nytmodultest.studieportal.R.*
 import dk.nytmodultest.studieportal.domain.model.Exercise
 import dk.nytmodultest.studieportal.domain.model.Questions
 import kotlinx.android.synthetic.main.activity_listening.*
@@ -20,7 +23,8 @@ import java.net.URL
 
 class Listening : AppCompatActivity(){
 
-    private var barPosition = 0
+    private var barPositionPct = 0
+    private var barTimeMillis = 0
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var seekBar: SeekBar
     private lateinit var runnable:Runnable
@@ -58,41 +62,29 @@ class Listening : AppCompatActivity(){
         override fun onPostExecute(result: String?) {
             //super.onPostExecute(result)
             val myresult = Gson().fromJson(result, Exercise::class.java)
-
             activity_listening_instruction.text = myresult.studentInstructions
 
-
-            //****************************************************************************
 
             //var mp3url: String = "http://10.0.2.2:8000/audio/01-sæ1_du3mo2_track1.mp3"//"localhost:8000/audio/" + myresult.media
             var mp3url: String = "http://s1download-universal-soundbank.com/mp3/sounds/9333.mp3"
 
-            //mediaPlayer.setAudioAttributes(AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA).setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build())
 
             mediaPlayer = MediaPlayer()
             mediaPlayer.setDataSource(mp3url)
             mediaPlayer.prepare()
 
-            //mSeekBar.setMax(mFileDuration); // where mFileDuration is mMediaPlayer.getDuration();
+            findViewById<TextView>(id.seekBarStart).text = "0"
+            findViewById<TextView>(id.seekBarEnd).text = (mediaPlayer.duration/1000).toString()
 
-
-
-
-            seekBar = findViewById<SeekBar>(dk.nytmodultest.studieportal.R.id.seekBar)
+            seekBar = findViewById<SeekBar>(id.seekBar)
             seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
                 override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                    //skriv kode
-                    barPosition = progress
+                    barPositionPct = progress
+                    barTimeMillis = mediaPlayer.currentPosition
                     if (fromUser){
                         mediaPlayer.seekTo(progress)
-
-
                     }
-                    if (mediaPlayer.getDuration() / 1000 == mediaPlayer.getCurrentPosition() / 1000) {
-
-                        return;
-                    }
-
+                    findViewById<TextView>(id.seekBarStart).text = (barTimeMillis/1000).toString()
 
                 }
                 override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -100,65 +92,36 @@ class Listening : AppCompatActivity(){
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar) {
-                    // Write code to perform some action when touch is stopped.
-                    //Toast.makeText(this@Listening,"StopedTracking!: ${barPosition}",Toast.LENGTH_SHORT).show()
-                    Toast.makeText(this@Listening,mp3url,Toast.LENGTH_SHORT).show()
-
 
                 }
             })
+
             seekBar.setMax(mediaPlayer.getDuration())
             //mSeekbarUpdateHandler.removeCallbacks(runnable);
-
-
 
 
             play_audiobtn.setOnClickListener{
 
                 if (!mediaPlayer.isPlaying){
-                    Toast.makeText(this@Listening,"lyd1",Toast.LENGTH_SHORT).show()
-                    //mediaPlayer.prepare()
-                    Toast.makeText(this@Listening,"lyd2",Toast.LENGTH_SHORT).show()
                     mSeekbarUpdateHandler.postDelayed(mUpdateSeekbar, 0);
                     mediaPlayer.start()
-                    Toast.makeText(this@Listening,"lyd3",Toast.LENGTH_SHORT).show()
                 }else{
-                    mediaPlayer.seekTo(0)
-                    //Toast.makeText(this@Listening,"lyd1",Toast.LENGTH_SHORT).show()
-                    //mediaPlayer.prepare()
-                    //Toast.makeText(this@Listening,"lyd2",Toast.LENGTH_SHORT).show()
-                    //mediaPlayer.start()
-                    //Toast.makeText(this@Listening,"lyd3",Toast.LENGTH_SHORT).show()
+                    mediaPlayer.pause()
 
                 }
-                //on pause:
-                //
-                //mSeekbarUpdateHandler.removeCallbacks(mUpdateSeekbar);
-
-
-                /*
-                if(pause){
-                    mediaPlayer.seekTo(mediaPlayer.currentPosition)
-                    mediaPlayer.start()
-                    pause = false
-                    //Toast.makeText(this,"media playing",Toast.LENGTH_SHORT).show()
-                }else{
-
-                    //mediaPlayer = MediaPlayer.create(applicationContext,R.raw.school_bell)
-                    mediaPlayer.start()
-                    //Toast.makeText(this,"media playing",Toast.LENGTH_SHORT).show()
-                }
-                */
 
             }
 
+            back_audiobtn.setOnClickListener{
+                mediaPlayer.seekTo(barTimeMillis-2000)
+            }
+
+            fwrd_audiobtn.setOnClickListener{
+                mediaPlayer.seekTo(barTimeMillis+2000)
+            }
 
 
-            //****************************************************************************
-
-
-
-            val recyclerView = findViewById(dk.nytmodultest.studieportal.R.id.activity_listening_recyclerView) as RecyclerView
+            val recyclerView = findViewById(id.activity_listening_recyclerView) as RecyclerView
             recyclerView.layoutManager = LinearLayoutManager(this@Listening, LinearLayout.VERTICAL, false)
 
             val questions = ArrayList<Questions>()
@@ -168,19 +131,12 @@ class Listening : AppCompatActivity(){
             val adapter = ListeningAdapter(questions, this@Listening)
             recyclerView.adapter = adapter
 
-
-
             activity_listening_confirmBtn.setOnClickListener{
                 val minListeAfKlicks = (recyclerView.adapter as ListeningAdapter).mineSvar.toString()
                 Toast.makeText(this@Listening,minListeAfKlicks,Toast.LENGTH_SHORT).show()
             }
 
-
-
-
         }
-
-
 
     }
 
@@ -192,7 +148,7 @@ class Listening : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(dk.nytmodultest.studieportal.R.layout.activity_listening)
+        setContentView(layout.activity_listening)
 
 
         //act_textcontent.setMovementMethod(ScrollingMovementMethod());
