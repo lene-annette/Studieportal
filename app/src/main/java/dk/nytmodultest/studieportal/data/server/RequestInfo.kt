9e97f34ed51e8d5f37d9ac4fe685cb9c6b17dc67
Weiqifan1 <cmlykke@hotmail.com>
2019-05-14 13:33:10 +0200

    changed listening.kt and requestInfo.kt to http://10.0.2.2:8000/

--- app/src/main/java/dk/nytmodultest/studieportal/data/server/RequestInfo.kt
+++ app/src/main/java/dk/nytmodultest/studieportal/data/server/RequestInfo.kt
@@ -3,6 +3,6 @@ package dk.nytmodultest.studieportal.data.server
 class RequestInfo {
 
     companion object{
-        const val API = "http://10.25.0.133:8000/api/" //"https://nytmodultest.dk/modultest-api/api/"
+        const val API = "http://10.0.2.2:8000/api/"//"http://10.25.0.133:8000/api/" //"https://nytmodultest.dk/modultest-api/api/"
     }
 }
